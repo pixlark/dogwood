47d6f0375e5c0ebe70f59027ade8d1ca1be14ca7
Brooke Tilley <brooke.tilley.2k@gmail.com>
2026-06-22 15:13:41 -0400

    converted typechecker to effects

--- src/Typechecker/Internal.hs
+++ src/Typechecker/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NamedFieldPuns #-}
@@ -10,13 +11,14 @@ import AST (AST (..), SyntaxTree (..))
 import qualified AST as A
 import Control.Applicative
 import Control.Monad
-import Control.Monad.Except
-import Control.Monad.Trans.Except
 import Data.List.NonEmpty (NonEmpty (..), (<|))
 import qualified Data.List.NonEmpty as NE
 import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as Text
+import Effectful (Eff, (:>))
+import Effectful.Error.Static
+import Effectful.State.Static.Local
 import Error
 import TypedAST (TST (..), typeOf)
 import qualified TypedAST as T
@@ -47,9 +49,10 @@ popScope :: LexicalScopes -> Maybe LexicalScopes
 popScope (_ :| []) = Nothing
 popScope (_ :| rest) = Just $ NE.fromList rest
 
-newtype Context = Context {scopes :: LexicalScopes}
+newtype Typechecker = Typechecker {scopes :: LexicalScopes}
 
-type Typechecker a = Except Err a
+-- type Typechecker a = Except Err a
+type TypecheckerE a = Eff '[State Typechecker, Error Err] a
 
 convertValueTypeExpr :: A.ValueTypeExpr -> T.ValueTypeExpr
 convertValueTypeExpr A.Void = T.Void
@@ -60,25 +63,28 @@ convertValueTypeExpr (A.NamespacedIdentifier parts) = T.NamespacedIdentifier par
 convertTypeExpr :: A.TypeExpr -> T.TypeExpr
 convertTypeExpr (A.TypeExpr {reference, valueExpr}) = T.TypeExpr {reference, valueExpr = convertValueTypeExpr valueExpr}
 
-typecheckExpr :: Context -> AST A.Expr -> Typechecker (TST T.Expr)
-typecheckExpr context (AST A.VoidLit span) = return $ TST T.VoidLit span
-typecheckExpr _ _ = undefined
+typecheckExpr :: AST A.Expr -> TypecheckerE (TST T.Expr)
+typecheckExpr (AST A.VoidLit span) = return $ TST T.VoidLit span
+typecheckExpr _ = undefined
 
 typeMismatch :: T.TypeExpr -> T.TypeExpr -> ErrorKind
 typeMismatch expected got = TypeMismatch {expected = Text.show expected, got = Text.show got}
 
-throwSpan :: Span -> ErrorKind -> Typechecker a
-throwSpan span kind = throwE $ Err kind span
+throwSpan :: Span -> ErrorKind -> TypecheckerE a
+throwSpan span kind = throwError $ Err kind span
 
 convertAST :: AST a -> TST a
 convertAST (AST a span) = TST a span
 
-typecheckStmt :: Context -> AST A.Stmt -> Typechecker (TST T.Stmt, Context)
-typecheckStmt (context@Context {scopes}) (AST (A.Let {name, type_, value}) span) = do
-  tValue <- typecheckExpr context value
+typecheckStmt :: AST A.Stmt -> TypecheckerE (TST T.Stmt)
+typecheckStmt (AST (A.Let {name, type_, value}) span) = do
+  scopes <- gets scopes
+  tValue <- typecheckExpr value
   let typeAnnotation = convertAST $ convertTypeExpr <$> type_
   let expectType = typeOf (node tValue)
   when (node typeAnnotation /= expectType) $ throwSpan (spanOf value) (typeMismatch (node typeAnnotation) expectType)
   let scopes' = bindNewVariable (node name) (node typeAnnotation) scopes
-  return $ (TST (T.Let {name = convertAST name, type_ = typeAnnotation, value = tValue}) span, Context {scopes = scopes'})
-typecheckStmt _ _ = undefined
+  typechecker <- get
+  put typechecker {scopes = scopes'}
+  return $ TST (T.Let {name = convertAST name, type_ = typeAnnotation, value = tValue}) span
+typecheckStmt _ = undefined
