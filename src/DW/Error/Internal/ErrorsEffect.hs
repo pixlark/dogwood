a112a163b5ea3a6ecaacbffc1942d42fbe7a28c9
Brooke Tilley <brooke.tilley.2k@gmail.com>
2026-07-04 16:39:32 -0400

    ErrorsEffect

--- src/DW/Error/Internal/ErrorsEffect.hs
+++ src/DW/Error/Internal/ErrorsEffect.hs
@@ -11,62 +11,11 @@ module DW.Error.Internal.ErrorsEffect where
 -- import Effectful.Dispatch.Dynamic
 
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
-import Data.Kind
-import Data.List (singleton)
 import Effectful
 import Effectful.Dispatch.Static
-import qualified Effectful.Error.Static as E
 import Effectful.Exception
 import Effectful.Internal.Utils
 import GHC.Stack
-
--- data Errors e :: Effect where
---   ThrowErr :: e -> Errors e m ()
---   MarkErr :: e -> Errors e m ()
-
--- type instance DispatchOf (Errors e) = Dynamic
-
--- -- | Throw an error, aborting all computation
--- throwErr :: (Errors e :> es) => e -> Eff es ()
--- throwErr e = send (ThrowErr e)
-
--- -- | Mark an error for later. The computation will eventually fail, but it
--- -- | continues on in case there are other errors to be accumulated.
--- markErr :: (Errors e :> es) => e -> Eff es ()
--- markErr e = send (MarkErr e)
-
--- runErrors :: (Show e) => Eff (Errors e : es) a -> Eff es (Either [(CallStack, e)] a)
--- runErrors = reinterpret evaluator $ \_ -> \case
---   ThrowErr e -> E.throwError e
---   MarkErr e -> undefined
---   where
---     evaluator e = do
---       E.runError e
-
--- data Errors e :: Effect
-
--- type instance DispatchOf (Errors e) = Static NoSideEffects
-
--------------------------------------------
-
--- data Errors e :: Effect
-
--- type instance DispatchOf (Errors e) = Static NoSideEffects
-
--- data ErrorsState e = ErrorsState {errors :: [e], continue :: Bool}
-
--- newtype instance StaticRep (Errors e) = Errors (ErrorsState e)
-
--- runErrors :: Eff (Errors e : es) a -> Eff es a
--- runErrors = evalStaticRep (Errors $ ErrorsState [] True)
-
--- throwErr :: (Errors e :> es) => e -> Eff es a
--- throwErr e = do
---   Errors (ErrorsState {errors}) <- getStaticRep
---   putStaticRep $ Errors $ ErrorsState {errors = e : errors, continue = False}
---   return ()
-
--------------------------------------------
 
 data Errors e :: Effect
 type instance DispatchOf (Errors e) = Static NoSideEffects
@@ -103,7 +52,9 @@ markErr e = do
   Errors (ErrorsState ref _) <- getStaticRep
   unsafeEff_ $ modifyIORef ref (e :)
 
--- Taken directly from effectful's implementation
+--
+-- The following is taken directly from effectful's implementation of Effectul.Error.Static
+--
 
 newtype ErrorId = ErrorId Unique
   deriving newtype (Eq)
@@ -132,68 +83,3 @@ matchError :: ErrorId -> ErrorWrapper -> Maybe (CallStack, e)
 matchError eid (ErrorWrapper etag cs _ e)
   | eid == etag = Just (cs, fromAny e)
   | otherwise = Nothing
-
--------------------------------------------
-
--- newtype Errors e a = Errors {runErrors_ :: [e] -> ([e], Maybe a)}
-
--- instance Functor (Errors e) where
---   fmap :: (a -> b) -> Errors e a -> Errors e b
---   fmap f x = Errors $ \e ->
---     let (errs', x') = runErrors_ x e
---      in (errs', f <$> x')
-
--- instance Applicative (Errors e) where
---   pure :: a -> Errors e a
---   pure x = Errors (,Just x)
-
---   (<*>) :: Errors e (a -> b) -> Errors e a -> Errors e b
---   f <*> x = Errors $ \e ->
---     let (e', f') = runErrors_ f e
---         (e'', x') = runErrors_ x e'
---      in (e'', f' <*> x')
-
--- instance Monad (Errors e) where
---   (>>=) :: Errors e a -> (a -> Errors e b) -> Errors e b
---   x >>= f = Errors $ \e ->
---     let (e', x') = runErrors_ x e
---      in case x' of
---           Nothing -> (e', Nothing)
---           Just x' -> runErrors_ (f x') e'
-
--- runErrors :: Errors e a -> Either [e] a
--- runErrors e = case (es, x) of
---   ([], Just x) -> Right x
---   ([], Nothing) -> error "unreachable"
---   (es, _) -> Left es
---   where
---     (es, x) = runErrors_ e []
-
--- throwErr :: e -> Errors e a
--- throwErr e = Errors $ \es -> (e : es, Nothing)
-
--- markErr :: e -> Errors e ()
--- markErr e = Errors $ \es -> (e : es, Just ())
-
------------------------------------
-
--- f <*> x = Errors $ runErrors f <*> runErrors x
-
--- instance Functor (Errors e) where
---   fmap :: (a -> b) -> Errors e a -> Errors e b
---   fmap f e = Errors $ f <$> runErrors e
-
--- instance Applicative (Errors e) where
---   pure :: a -> Errors e a
---   pure = Errors . Right
---   (<*>) :: Errors e (a -> b) -> Errors e a -> Errors e b
---   f <*> x = Errors $ runErrors f <*> runErrors x
-
--- instance Monad (Errors e) where
---   x >>= f =
---     let x' = runErrors x
---         fx = x' >>= (runErrors . f)
---      in Errors fx
-
--- throwErr :: e -> Errors e a
--- throwErr = Errors . Left . singleton
--- test/ErrorsSpec.hs
+++ test/ErrorsSpec.hs
@@ -2,14 +2,17 @@
 
 module ErrorsSpec (spec) where
 
-import DW.Common (runPureEff)
 import DW.Error.Internal.ErrorsEffect
+import Data.IORef
+import Effectful
+import Effectful.Dispatch.Static (unsafeEff_)
+import Effectful.Labeled (labeled, runLabeled)
 import Test.Hspec
 
+{- HLINT ignore "Use let" -}
 spec =
   describe "the Errors effect" do
     it "can abort computations" do
-      {- HLINT ignore "Use let" -}
       let
         e :: Either [String] Int
         e = runPureEff $ runErrors do
@@ -17,6 +20,7 @@ spec =
           _ <- return (2 :: Int)
           throwErr "yo what's up"
       e `shouldBe` Left ["yo what's up"]
+
     it "can log errors and keep going" do
       let
         e :: Either [String] Int
@@ -26,6 +30,7 @@ spec =
           _ <- return (2 :: Int)
           throwErr "now we gotta abort"
       e `shouldBe` Left ["woah that's bad", "now we gotta abort"]
+
     it "fails if any errors are logged" do
       let
         e :: Either [String] Int
@@ -42,3 +47,117 @@ spec =
           y <- return 2
           return $ x + y
       e `shouldBe` Left ["here's an error"]
+
+    it "preserves error order" do
+      let
+        e :: Either [String] ()
+        e = runPureEff $ runErrors do
+          markErr "first"
+          markErr "second"
+          markErr "third"
+      e `shouldBe` Left ["first", "second", "third"]
+
+    it "handles many accumulated errors" do
+      let
+        e :: Either [Int] ()
+        e = runPureEff $ runErrors do
+          mapM_ markErr [1 .. 100]
+      e `shouldBe` Left [1 .. 100]
+
+    it "does not execute code after throwErr" do
+      ref <- newIORef False
+      let
+        e :: Either [String] ()
+        e = runPureEff $ runErrors do
+          _ <- throwErr "abort"
+          unsafeEff_ $ writeIORef ref True
+      e `shouldBe` Left ["abort"]
+      readIORef ref `shouldReturn` False
+
+    it "does execute code after markErr" do
+      ref <- newIORef False
+      let
+        e :: Either [String] ()
+        e = runPureEff $ runErrors do
+          markErr "continue"
+          unsafeEff_ $ writeIORef ref True
+      e `shouldBe` Left ["continue"]
+      readIORef ref `shouldReturn` True
+
+    it "collects errors before throwErr" do
+      let
+        e :: Either [String] Int
+        e = runPureEff $ runErrors do
+          markErr "first"
+          markErr "second"
+          _ <- throwErr "abort"
+          markErr "never reached"
+          return 1
+      e `shouldBe` Left ["first", "second", "abort"]
+
+    it "handles nested runErrors independently" do
+      let
+        e :: Either [String] (Either [String] Int)
+        e = runPureEff $ runLabeled @"outer" runErrors do
+          labeled @"outer" $ markErr "outer"
+          inner <- runLabeled @"inner" runErrors do
+            labeled @"inner" $ markErr "inner"
+            return (42 :: Int)
+          return inner
+      e `shouldBe` Left ["outer"]
+
+    it "nested runErrors don't interfere with each other" do
+      let
+        e :: Either [String] (Either [Int] Int, Either [Int] Int)
+        e = runPureEff $ runErrors do
+          r1 <- runErrors @Int do
+            markErr 1
+            return 10
+          r2 <- runErrors @Int do
+            return 20
+          return (r1, r2)
+      e `shouldBe` Right (Left [1], Right 20)
+
+    it "inner throwErr doesn't affect outer" do
+      let
+        e :: Either [String] (Either [String] Int)
+        e = runPureEff $ runLabeled @"outer" runErrors do
+          inner <- runLabeled @"inner" runErrors do
+            labeled @"inner" $ throwErr "inner abort"
+          return inner
+      e `shouldBe` Right (Left ["inner abort"])
+
+    it "outer throwErr aborts before inner runs" do
+      let
+        e :: Either [String] (Either [String] Int)
+        e = runPureEff $ runErrors do
+          _ <- throwErr "outer abort"
+          runErrors do
+            return 42
+      e `shouldBe` Left ["outer abort"]
+
+    it "handles empty computation" do
+      let
+        e :: Either [String] ()
+        e = runPureEff $ runErrors $ return ()
+      e `shouldBe` Right ()
+
+    it "computation result is discarded when errors exist" do
+      let
+        e :: Either [String] Int
+        e = runPureEff $ runErrors do
+          markErr "error"
+          return 42
+      e `shouldBe` Left ["error"]
+
+    it "multiple throwErr only throws first" do
+      ref <- newIORef (0 :: Int)
+      let
+        e :: Either [String] ()
+        e = runPureEff $ runErrors do
+          unsafeEff_ $ modifyIORef ref (+ 1)
+          _ <- throwErr "first"
+          unsafeEff_ $ modifyIORef ref (+ 1)
+          throwErr "second"
+      e `shouldBe` Left ["first"]
+      readIORef ref `shouldReturn` 1
